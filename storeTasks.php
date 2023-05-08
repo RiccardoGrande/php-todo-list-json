6a43ffe9ca20dae23b9091688ba2b13a4f61073b
RiccardoGrande <grandericcardo1997@gmail.com>
2023-05-08 16:52:34 +0200

    add php

--- storeTasks.php
+++ storeTasks.php
@@ -1,11 +1,10 @@
 <?php
-$file_path = '../../../../tasks.json';
 
 if (isset($_POST['new_task'])) {
     $task = $_POST['new_task'];
 
     // read the json file with file_get_contents
-    $tasks_string = file_get_contents($file_path);
+    $tasks_string = file_get_contents('tasks.json');
     // convert the json_string into an associative array with json_decode()
     $tasks_array = json_decode($tasks_string, true);
     //add the new task to the array
@@ -14,12 +13,9 @@ if (isset($_POST['new_task'])) {
     // convert the array back into a json string
     $new_tasks_json_string = json_encode($tasks_array);
     // replace the file content using file_put_contents()
-    file_put_contents($file_path, $new_tasks_json_string);
+    file_put_contents('tasks.json', $new_tasks_json_string);
     // add header application/json
     header('Content-Type: application/json');
-
-    header("Access-Control-Allow-Origin: http://localhost:5173");
-    header("Access-Control-Allow-Headers: X-Requested-With");
     // echo json
     echo $new_tasks_json_string;
 }
